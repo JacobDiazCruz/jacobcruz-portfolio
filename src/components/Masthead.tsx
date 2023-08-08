d2518ff07e4cb07a479d4187dcb0aade97f12de9
JacobDiazCruz <carljacobdiazcruz@gmail.com>
2023-08-08 23:49:57 +0800

    Added: banner chat

--- src/components/Masthead.tsx
+++ src/components/Masthead.tsx
@@ -1,6 +1,6 @@
 export default function Masthead() {
   return (
-    <>
+    <div className="flex items-center gap-[100px]">
       <div className="w-full">
         <div className="font-medium text-[22px]">
           Jacob Cruz
@@ -11,7 +11,7 @@ export default function Masthead() {
         <h1 className="md:text-[62px] text-[36px] mt-7 text-neutral-300 font-light md:leading-[65px]">
           Building great <span className="italic text-white font-normal">apps</span> to empower people
         </h1>
-        <p className="md:text-[24px] font-light text-[20px] text-[#ACACAC] mt-7">
+        <p className="md:text-[24px] font-light text-[20px] text-neutral-200 mt-7">
           A software engineer based in Manila, Philippines
         </p>
         <button className="py-2 px-4 rounded-md ml-0 mt-7 text-[20px] font-normal cursor-pointer bg-[#7FEE91] text-[#003308] border-none">
@@ -19,7 +19,15 @@ export default function Masthead() {
         </button>
       </div>
       <div className="w-[700px] md:block hidden">
+        <div className="w-fit p-3 border bg-neutral-900 border-s-neutral-500 border-y-neutral-700 border-r-neutral-800 rounded-lg font-light">
+          <span className="text-green-400">request: </span>
+          <span className="text-violet-400 ml-1">&lsquo;Work with me&rsquo;</span>;
+        </div>
+        <div className="w-fit p-3 mt-3 ml-5 border bg-neutral-900 border-s-neutral-500 border-y-neutral-700 border-r-neutral-800 rounded-lg font-light">
+          <span className="text-green-400">response: </span>
+          <span className="text-yellow-100 ml-1">&lsquo;Sure, when will we start?&rsquo;</span>;
+        </div>
       </div>
-    </>
+    </div>
   );
 }
