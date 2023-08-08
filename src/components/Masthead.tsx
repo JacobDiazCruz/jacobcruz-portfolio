62ff00228917780bd428c06a89ef072d4b13a71b
JacobDiazCruz <carljacobdiazcruz@gmail.com>
2023-08-09 00:05:47 +0800

    Update: icon

--- src/components/Masthead.tsx
+++ src/components/Masthead.tsx
@@ -6,13 +6,13 @@ export default function Masthead() {
           Jacob Cruz
         </div>
         <div className="text-neutral-300">
-          Developer + <span className="font-mono italic">Designer</span>
+          Senior Software Engineer
         </div>
         <h1 className="md:text-[62px] text-[36px] mt-7 text-neutral-300 font-light md:leading-[65px]">
           Building great <span className="italic text-white font-normal">apps</span> to empower people
         </h1>
         <p className="md:text-[24px] font-light text-[20px] text-neutral-200 mt-7">
-          A software engineer based in Manila, Philippines
+          Based in Manila, Philippines
         </p>
         <button className="py-2 px-4 rounded-md ml-0 mt-7 text-[20px] font-normal cursor-pointer bg-[#7FEE91] text-[#003308] border-none">
           contact me
@@ -21,7 +21,7 @@ export default function Masthead() {
       <div className="w-[700px] md:block hidden">
         <div className="w-fit p-3 border bg-neutral-900 border-s-neutral-500 border-y-neutral-700 border-r-neutral-800 rounded-lg font-light">
           <span className="text-green-400">request: </span>
-          <span className="text-violet-400 ml-1">&lsquo;Work with me&rsquo;</span>;
+          <span className="text-violet-400 ml-1">&lsquo;Let&rsquo;s work together&rsquo;</span>;
         </div>
         <div className="w-fit p-3 mt-3 ml-5 border bg-neutral-900 border-s-neutral-500 border-y-neutral-700 border-r-neutral-800 rounded-lg font-light">
           <span className="text-green-400">response: </span>
