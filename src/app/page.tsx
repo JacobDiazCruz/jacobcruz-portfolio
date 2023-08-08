1564823611423c50c98924395376f1b65e95fda0
JacobDiazCruz <carljacobdiazcruz@gmail.com>
2023-08-08 16:51:48 +0800

    Update favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,16 @@ import Footer from "@/components/Footer";
 import Masthead from "@/components/Masthead";
 import Section from "@/components/Section";
 import WorkExperience from "@/components/WorkExperience";
-import Head from "next/head"
 import CjLogo from "../../public/cj-logo.svg";
+
+export const metadata = {
+  title: 'Jacob Cruz - Software Engineer',
+  description: ''
+}
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Jacob Cruz - Software Engineer</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      
       <main className="p-6 bg-[#1B1B1B] w-full">
         <div className="flex items-center m-[0 auto] gap-[10px] p-2">
           <div className="w-[181px] h-[86px] bg-[#37682B] absolute rounded-full z-10 filter blur-[95px]">asd</div>
